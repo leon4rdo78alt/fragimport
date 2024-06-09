5bb4d70d69868d0d7feafd0eb790ff74bdb06da1
leon4rdo78 <95756688+leon4rdo78@users.noreply.github.com>
2024-06-09 22:54:14 +0330

    Update random_lines.py

--- selector/random_lines.py
+++ selector/random_lines.py
@@ -1,15 +1,16 @@
 import random
 
 # File names
-input_file = 'list'
+input_file_1 = 'list'
+input_file_2 = 'Channels'
 output_file = 'selector/random'
 
 # Number of lines to select
 num_lines_to_select = 10
 
-def select_random_lines(input_file, output_file, num_lines):
-    with open(input_file, 'r') as f:
-        lines = f.readlines()
+def select_random_lines(input_file_1, input_file_2, output_file, num_lines):
+    with open(input_file_1, 'r') as f1, open(input_file_2, 'r') as f2:
+        lines = f1.readlines() + f2.readlines()
 
     # Ensure we do not exceed the number of available lines
     num_lines = min(num_lines, len(lines))
@@ -21,4 +22,4 @@ def select_random_lines(input_file, output_file, num_lines):
         f.writelines(selected_lines)
 
 if __name__ == "__main__":
-    select_random_lines(input_file, output_file, num_lines_to_select)
+    select_random_lines(input_file_1, input_file_2, output_file, num_lines_to_select)
