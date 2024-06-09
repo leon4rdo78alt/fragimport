8c53f2f90196c4d785f5dc2a1871f7afd2c98a62
leon4rdo78 <95756688+leon4rdo78@users.noreply.github.com>
2024-06-09 23:46:32 +0330

    Update random_lines.py

--- selector/random_lines.py
+++ selector/random_lines.py
@@ -1,33 +1,25 @@
+
 import random
 
 # File names
-input_file_1 = 'list'
-input_file_2 = 'gitChannels'
+input_file = 'list'
 output_file = 'selector/random'
 
 # Number of lines to select
 num_lines_to_select = 10
 
-def select_random_lines(input_file_1, input_file_2, output_file, num_lines):
-    selected_lines = set()  # Set to keep track of selected lines
+def select_random_lines(input_file, output_file, num_lines):
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
 
-    # Read lines from first file
-    with open(input_file_1, 'r') as f1:
-        lines_1 = set(f1.readlines())
-
-    # Read lines from second file
-    with open(input_file_2, 'r') as f2:
-        lines_2 = set(f2.readlines())
-
-    # Randomly sample lines from the first file
-    selected_lines.update(random.sample(lines_1, min(num_lines, len(lines_1))))
-
-    # Randomly sample lines from the second file
-    selected_lines.update(random.sample(lines_2, min(num_lines - len(selected_lines), len(lines_2))))
-
-    # Write selected lines to output file
+    # Ensure we do not exceed the number of available lines
+    num_lines = min(num_lines, len(lines))
+    
+    # Randomly sample lines without replacement
+    selected_lines = random.sample(lines, num_lines)
+    
     with open(output_file, 'w') as f:
         f.writelines(selected_lines)
 
 if __name__ == "__main__":
-    select_random_lines(input_file_1, input_file_2, output_file, num_lines_to_select)
+    select_random_lines(input_file, output_file, num_lines_to_select)
