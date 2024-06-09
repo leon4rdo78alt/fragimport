f28cfdfc33b54567126dcda97783003ca2439c53
leon4rdo78 <95756688+leon4rdo78@users.noreply.github.com>
2024-06-09 22:56:30 +0330

    Update random_lines.py

--- selector/random_lines.py
+++ selector/random_lines.py
@@ -9,15 +9,23 @@ output_file = 'selector/random'
 num_lines_to_select = 10
 
 def select_random_lines(input_file_1, input_file_2, output_file, num_lines):
-    with open(input_file_1, 'r') as f1, open(input_file_2, 'r') as f2:
-        lines = f1.readlines() + f2.readlines()
+    selected_lines = set()  # Set to keep track of selected lines
 
-    # Ensure we do not exceed the number of available lines
-    num_lines = min(num_lines, len(lines))
-    
-    # Randomly sample lines without replacement
-    selected_lines = random.sample(lines, num_lines)
-    
+    # Read lines from first file
+    with open(input_file_1, 'r') as f1:
+        lines_1 = set(f1.readlines())
+
+    # Read lines from second file
+    with open(input_file_2, 'r') as f2:
+        lines_2 = set(f2.readlines())
+
+    # Randomly sample lines from the first file
+    selected_lines.update(random.sample(lines_1, min(num_lines, len(lines_1))))
+
+    # Randomly sample lines from the second file
+    selected_lines.update(random.sample(lines_2, min(num_lines - len(selected_lines), len(lines_2))))
+
+    # Write selected lines to output file
     with open(output_file, 'w') as f:
         f.writelines(selected_lines)
 
