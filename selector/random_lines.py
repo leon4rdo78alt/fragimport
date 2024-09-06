6d3531e01a562a9c32c65b58d835efc6406e316e
kofukuDESU <95756688+leon4rdo78@users.noreply.github.com>
2024-09-06 13:23:41 +0330

    Update random_lines.py

--- selector/random_lines.py
+++ selector/random_lines.py
@@ -1,4 +1,3 @@
-
 import random
 
 # File names
@@ -12,11 +11,12 @@ def select_random_lines(input_file, output_file, num_lines):
     with open(input_file, 'r') as f:
         lines = f.readlines()
 
-    # Ensure we do not exceed the number of available lines
-    num_lines = min(num_lines, len(lines))
-    
-    # Randomly sample lines without replacement
-    selected_lines = random.sample(lines, num_lines)
+    # If there are fewer lines than requested, use all lines
+    if len(lines) <= num_lines:
+        selected_lines = lines
+    else:
+        # Randomly sample lines without replacement
+        selected_lines = random.sample(lines, num_lines)
     
     with open(output_file, 'w') as f:
         f.writelines(selected_lines)
